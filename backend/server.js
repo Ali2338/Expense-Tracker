a6857e74971bbfad7c111df1c90ebd9e0b0e3363
Mahima Khatri <mahikhatri2004@gmail.com>
2025-04-16 22:47:51 +0530

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
+const incomeRoutes = require('./routes/incomeRoutes');
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET","POST","PUT","DELETE"],
@@ -16,7 +18,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/v1/auth',authRoutes);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/v1/income',incomeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
