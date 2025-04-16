ee3dc181e1eead1cffb401306f283de9b082f43a
Mahima Khatri <mahikhatri2004@gmail.com>
2025-04-16 22:50:41 +0530

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-console.log("MONGO_URI from .env:", process.env.MONGO_URI);
+//console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
